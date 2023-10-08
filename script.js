ff0ce0fa124459b4bf256aca3d6d0c6928187cf3
MADESH122 <madeshwaran9080@gmail.com>
2023-10-08 16:22:13 +0530

    push

--- script.js
+++ script.js
@@ -13,19 +13,17 @@ var res=fetch("https://restcountries.com/v2/all")
     for(i=0;i<data2.length;i++){
         console.log(data2[i])
         row.innerHTML+=`
-<div class="col-md-4">
-<div class="card border-dark mb-3" style="max-width: 18rem;">
-<div class="card-header"><h2>${data2[i].name}</h2></div>
-<div class="card-body text-dark">
-<h5 class="card-title">${data2[i].flags.svg}</h5>
-<h5 class="card-title">Capital : ${data2[i].capital}</h5>
-<h5 class="card-title">Region : ${data2[i].region}</h5>
-<h5 class="card-title">CallingCodes : ${data2[i].callingCodes}</h5>
- </div>
-</div>
-</div>`
+        <div class = "col-md-4 text-center">
+        <div class="card border-dark mb-3" style="max-width: 18rem;">
+       <div class="card-header text-white bg-dark">${data2[i].name}</div>
+       <div class="card-body text-dark">
+       <img src=${data2[i].flags.png} class="card-img-top" alt="...">
+       <p class="card-text">Capital:${data2[i].capital}</p>
+       <p class="card-text">Region:${data2[i].region}</p>
+       <p class="card-text">LatLong:${data2[i].latlng}</p>
+       </div>
+       </div>`
     }
-    
   
 })
 
